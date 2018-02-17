8838f7f0da45cf4c003fcde3a1dd3efb79e1b592
ravikiradoo <kiradooravi@gmail.com>
2018-02-17 18:26:02 +0530

    Create Calculator Layout

---
+++ main.py
@@ -0,0 +1,53 @@
+import sys
+from PyQt5.QtWidgets import*
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class Application(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calculator")
+        self.StartApp()
+
+    def StartApp(self):
+        grid=QGridLayout()
+
+        results=QLineEdit()
+        grid.addWidget(results,0,0,1,4)
+
+        buttons=["AC","C","CE","/",
+                "7","8","9","*",
+                "4","5","6","-",
+                "1","2","3","+",
+                "0",".","="
+                ]
+
+
+
+
+        row=1
+        col=0
+        for button in buttons:
+            if col>3:
+                row+=1
+                col=0
+
+
+            if button=="0":
+                grid.addWidget(QPushButton(str(button)),row,col,1,2)
+                col+=2
+            else:
+                grid.addWidget(QPushButton(str(button)), row, col, 1, 1)
+                col += 1
+
+        self.setLayout(grid)
+
+        self.show()
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    window=Application()
+    sys.exit(app.exec_())
+
+
+
